426b971acc0a16b3a43fbff05b3c73a5d933cb08
Titoxox97 <87781987+Titoxox97@users.noreply.github.com>
2021-10-03 17:29:37 -0400

    made function that returns license section of the ReadMe

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,6 +24,7 @@ function renderLicenseBadge(license) {
       return '';
     }
   };
+  return licBadge[license]();
 }
 
 // TODO: Create a function that returns the license link
@@ -32,7 +33,38 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let section = {
+    'Apache 2.0': function () {
+      return `## License
+      [![licBadge](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) <br /> Application is backed by ${license} license.`;
+    },
+    'bsl-1.0': function () {
+      return `## License
+      [![licBadge](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt) <br /> Application is backed by ${license} license.`;
+    },
+    'CC': function () {
+      return `## License
+      [![licBadge](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/) <br /> Application is backed by ${license} license.`;
+    },
+    'ISC': function () {
+      return `## License
+      [![licBadge](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC) <br /> Application is backed by ${license} license.`;
+    },
+    'MIT': function () {
+      return `## License
+      [![licBadge](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) <br /> Application is backed by ${license} license.`;
+    },
+    'Mozilla 2.0': function () {
+      return `# License
+      [![licBadge](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) <br /> Application is backed by ${license} license.`;
+    },
+    'None': function () {
+      return '';
+    }
+  };
+  return section[licBadge]();
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
